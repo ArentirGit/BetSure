33691132d4df06378ad93bf699bed66d8be74b16
ArentirGit <arentir.contact@gmail.com>
2016-01-27 15:52:27 +0100

    BetSureV1.A2.A1.3.3
    functionnal commit

--- src/BS/MainBundle/Controller/MainController.php
+++ src/BS/MainBundle/Controller/MainController.php
@@ -10,11 +10,12 @@ class MainController extends Controller
     public function mainAction()
     {
         ini_set('max_execution_time', 600); //300 seconds = 5 minutes
+        ini_set('memory_limit', '-1');
         $this->forward('BSOfferBundle:Offer:get');
         $this->forward('BSResultBundle:Result:get');
         $this->forward('BSBetBundle:Bet:home');
         $this->forward('BSResultBundle:Result:offerToResult');
-        $this->forward('BSRBetBundle:Bet:update');
+        $this->forward('BSBetBundle:Bet:update');
 
         return new Response("Hello World");
     }
